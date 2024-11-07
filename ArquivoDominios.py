2bb41ca8ecebfe732b7c3b4430382e5e6faf9298
Clenylson Perycles <clenylson.perycles@gmail.com>
2024-11-07 17:11:59 -0300

    Commit teste

--- ArquivoDominios.py
+++ ArquivoDominios.py
@@ -373,3 +373,4 @@ for nota in NOTAS:
                                                 REGISTRO2081["SUB_SERIE"],
                                                 REGISTRO2081["EMITENTE"],
                                                 REGISTRO2081["CHAVE_NFE_CTE_SAIDA"])
+                                                
